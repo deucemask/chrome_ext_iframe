4a176c211077447b4285f3e239d610371fff9a4f
dennis <minskparisminsk@gmail.com>
2015-07-27 15:44:58 -0700

    added more console messaging

--- src/rm.js
+++ src/rm.js
@@ -1,20 +1,14 @@
-var removalScript = "var i, frames = document.getElementsByTagName('iframe'), nodes = []; "
+var removalScript = "var i, nodes = [], frames, count = 0;"
++ "frames = document.getElementsByTagName('iframe');"
++ "count = frames.length;"
 + "for (i = 0; i < frames.length; i++) { nodes[i] = frames.item(i); }; "
 + "for (i = 0; i < nodes.length; i++) { nodes[i].parentNode.removeChild(nodes[i]); };";
 
+var msgDone = "console.log(count + ' elements were removed.');";
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
+  var msg = "console.log('Removing DOM elements from " +  tab.url + "'); ";
   chrome.tabs.executeScript({
-    code: 'console.log("Removing iframes from ' +  tab.url + '"); ' + removalScript
+    code: msg + removalScript + msgDone
   });
 });
-
-
-//chrome.browserAction.onClicked.addListener(function(tab) {
-//  chrome.tabs.executeScript({
-//    code: "console.log('removing iframes')"
-//  });
-//});
-
-
