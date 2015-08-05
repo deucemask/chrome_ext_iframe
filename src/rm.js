aee51db10d40e6769b982a8712c56a5c7d7f286e
dennis <minskparisminsk@gmail.com>
2015-08-04 22:05:38 -0700

    support script injection in child frames

--- src/rm.js
+++ src/rm.js
@@ -1,14 +1,9 @@
-var removalScript = "var i, nodes = [], frames, count = 0;"
-+ "frames = document.getElementsByTagName('iframe');"
-+ "count = frames.length;"
-+ "for (i = 0; i < frames.length; i++) { nodes[i] = frames.item(i); }; "
-+ "for (i = 0; i < nodes.length; i++) { nodes[i].parentNode.removeChild(nodes[i]); };";
+console.log('Removing DOM elements from ' +  window.location.href);
 
-var msgDone = "console.log(count + ' elements were removed.');";
+var i, nodes = [], frames, count = 0;
+frames = document.getElementsByTagName('iframe');
+count = frames.length;
+for (i = 0; i < frames.length; i++) { nodes[i] = frames.item(i); };
+for (i = 0; i < nodes.length; i++) { nodes[i].parentNode.removeChild(nodes[i]); };
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  var msg = "console.log('Removing DOM elements from " +  tab.url + "'); ";
-  chrome.tabs.executeScript({
-    code: msg + removalScript + msgDone
-  });
-});
+console.log(count + ' elements were removed.');
